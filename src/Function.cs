5d0d9952055e00f21a220ef9cc272d5e5f700f76
Dennis Playdon <dennis.playdon@xero.com>
2021-07-20 18:34:52 +1200

    Add ability to run lambda as a console app

--- src/Function.cs
+++ src/Function.cs
@@ -5,7 +5,17 @@ namespace SurfReport
 {
     public class Function
     {
+        static async Task Main(string[] args)
+        {
+            await Handle();
+        }
+        
         public async Task FunctionHandler()
+        {
+            await Handle();
+        }
+
+        private static async Task Handle()
         {
             var reportAnalyser = new SurfReportAnalyser();
             var idealSurfReports = await reportAnalyser.Analyse();
